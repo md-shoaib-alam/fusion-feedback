92e7a13689c8a13a0d655d8ba24e3d952c040aff
md-shoaib-alam <shoaibsalam0786@gmail.com>
2025-09-05 10:00:00 +0000

    setup cleark

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
 
@@ -17,19 +18,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter} min-h-screen flex flex-col`}>
-        {/* Navbar  */}
-        <Navbar />
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${inter} min-h-screen flex flex-col`}>
+          {/* Navbar  */}
+          <Navbar />
 
-        {/* Main Section */}
-        <main className="flex-1 container mx-auto px-4 py-8"> {children}</main>
+          {/* Main Section */}
+          <main className="flex-1 container mx-auto px-4 py-8"> {children}</main>
 
-        {/* Footer  */}
-        <Footer />
-        <Toaster />
+          {/* Footer  */}
+          <Footer />
+          <Toaster />
 
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
